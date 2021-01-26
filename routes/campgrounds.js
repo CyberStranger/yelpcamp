1af4daea55ec78cb1eca24a74564c4e9cc987d50
Dubinin Maxim <cyberstranger@yandex.ru>
2021-01-26 21:38:56 +0300

    Added routes in separate files

---
+++ routes/campgrounds.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError');
+const Campground = require('../models/campground');
+const { campgroundSchema } = require('../schemas.js')
+
+const router = express.Router();
+
+
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body)
+  if (error) {
+    const message = error.details.map(el => el.message).join(',');
+    throw new ExpressError(message, 400);
+  } else {
+    next();
+  }
+}
+
+
+router.get('/new', (req, res) => {
+  res.render('campgrounds/new');
+})
+
+router.post('/', validateCampground, catchAsync(async (req, res) => {
+
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+
+}));
+
+router.get('/', catchAsync(async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', { campgrounds });
+}));
+
+router.get('/:id', catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id).populate('reviews');
+  res.render('campgrounds/show', { campground });
+}));
+
+router.get('/:id/edit', catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render('campgrounds/edit', { campground });
+}));
+
+router.put('/:id', validateCampground, catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+  res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+router.delete('/:id', catchAsync(async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect('/campgrounds')
+}));
+
+module.exports = router;
+
