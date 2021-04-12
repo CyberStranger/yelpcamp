823638095aa263500b777b3db24c8fbe317f1ec1
Dubinin Maxim <cyberstranger@yandex.ru>
2021-04-12 17:21:26 +0300

    Routes has been refactored. Added controllers for models

--- routes/users.js
+++ routes/users.js
@@ -2,44 +2,19 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
-const User = require('../models/user');
+const users = require('../controllers/users');
 
-router.get('/register', (req, res) => {
-  res.render('users/register');
-})
+router.route('/register')
+  .get(users.renderRegister)
+  .post(catchAsync(users.register));
 
-router.post('/register', catchAsync(async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    const user = new User({ username, email });
-    const registeredUser = await User.register(user, password);
-    req.login(registeredUser, err => {
-      if (err) return next(err);
-      req.flash('success', `Welcome to Yelp Camp, ${registeredUser.username}`);
-      res.redirect('/campgrounds');
-    });
+router.route('/login')
+  .get(users.renderLoginForm)
+  .post(passport.authenticate('local', {
+    failureFlash: true,
+    failureRedirect: '/login'
+  }), users.login);
 
-  } catch (err) {
-    req.flash('error', err.message);
-    return res.redirect('/register')
-  }
-}));
-
-router.get('/login', (req, res) => {
-  res.render('users/login');
-});
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-  req.flash('success', 'Welcome back!');
-  const redirectUrl = req.session.returnTo || '/campgrounds'
-  delete req.session.returnTo;
-  res.redirect(redirectUrl);
-});
-
-router.get('/logout', (req, res) => {
-  req.logout();
-  req.flash('success', 'Goodbye')
-  res.redirect('/campgrounds');
-})
+router.get('/logout', users.logout)
 
 module.exports = router;
