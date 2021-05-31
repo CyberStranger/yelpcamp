5e92c5bf9e545daba4618a99aaf5177bb786bf7e
Dubinin Maxim <cyberstranger@yandex.ru>
2021-05-31 16:25:17 +0300

    added multer middleware for parsing multipart form data

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,10 +3,15 @@ const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware')
 const router = express.Router();
 const campgrounds = require('../controllers/campgrounds');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 
 router.route('/')
   .get(catchAsync(campgrounds.index))
-  .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+  // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+  .post(upload.array('image'),(req,res) => {
+    res.send(req.body, req.files);
+  })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
