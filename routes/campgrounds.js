87dc78608fbebf7d7977709c7bc672ea4936a467
Dubinin Maxim <cyberstranger@yandex.ru>
2021-06-06 20:40:51 +0300

    Добавлена возможность загрузки изображений на сервис cloudinary fix страницы просмотра турбазы

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,14 +4,13 @@ const { isLoggedIn, isAuthor, validateCampground } = require('../middleware')
 const router = express.Router();
 const campgrounds = require('../controllers/campgrounds');
 const multer = require('multer');
-const upload = multer({dest: 'uploads/'});
+const { storage } = require('../cloudinary'); //import cloudinary storage
+const upload = multer({ storage });
 
 router.route('/')
   .get(catchAsync(campgrounds.index))
-  // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-  .post(upload.array('image'),(req,res) => {
-    res.send(req.body, req.files);
-  })
+  .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
+  
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
